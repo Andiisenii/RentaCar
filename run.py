d6dc913eac93b021de21f8f12b9e8cda8e6802d2
Andi Iseni <ai30978@seeu.edu.mk>
2025-06-06 00:24:41 +0200

    Ensure Flask-Login is available and fix Render deploy

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from app import create_app, db
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # krijon tabelat në databazë
-    app.run(debug=True, port=5001)
+        db.create_all()
+
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, port=port)

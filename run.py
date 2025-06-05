b97f7ad31dabb39b6597677e8ca46b07ad7d546f
Andi Iseni <ai30978@seeu.edu.mk>
2025-06-06 00:32:21 +0200

    Fix case-sensitive Flask-Login module

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
+# run.py
 from app import create_app, db
 import os
 
 app = create_app()
 
+# Kjo pjesë do të ekzekutohet vetëm gjatë zhvillimit lokal
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    port = int(os.environ.get('PORT', 5001))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get('PORT', 5000))  # 5000 default, Render e cakton ndryshe
+    app.run(host='0.0.0.0', port=port)

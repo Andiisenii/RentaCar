010ea75eea766754600ed461e39a2c87c9a3e07a
Andi Iseni <ai30978@seeu.edu.mk>
2025-06-08 21:44:52 +0200

    prapr update

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 db = SQLAlchemy()
 migrate = Migrate()
 
